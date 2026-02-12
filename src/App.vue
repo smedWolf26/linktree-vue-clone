b21966860f0826758bd9ed8af28af0ec3e8a4f6e
smedWolf26 <smedina.lambda@gmail.com>
2026-02-12 11:17:56 -0600

    Changed app.vue layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex min-h-screen flex-col">
+  <div class="from-grey-900 min-h-screen bg-linear-to-br via-gray-800 to-gray-900 font-sans">
     <NavMenu />
     <RouterView />
   </div>
